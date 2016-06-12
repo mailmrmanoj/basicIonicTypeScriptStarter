eaee60b29f89dab936ac49da35619c32d8cd938a
Manoj <mailmrmanoj@gmail.com>
2016-06-12 12:21:37 +0530

    test

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -264,21 +264,6 @@ var AngularAttack;
     (function (Controllers) {
         var CheckinCtrl = (function () {
             function CheckinCtrl($scope, $stateParams, Chats) {
-                $scope.showForm = true;
-                $scope.shirtSizes = [
-                    { text: 'Large', value: 'L' },
-                    { text: 'Medium', value: 'M' },
-                    { text: 'Small', value: 'S' }
-                ];
-                $scope.attendee = {};
-                $scope.submit = function () {
-                    if (!$scope.attendee.firstname) {
-                        alert('Info required');
-                        return;
-                    }
-                    $scope.showForm = false;
-                    $scope.attendees.push($scope.attendee);
-                };
             }
             CheckinCtrl.$inject = ["$scope", "$stateParams", "Chats"];
             return CheckinCtrl;
